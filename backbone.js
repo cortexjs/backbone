a1fefa31581b65df20685663feb4001db1a4f630
mangix <maqh1988@gmail.com>
2013-12-27 13:56:12 +0800

    backbone

--- backbone.js
+++ backbone.js
@@ -6,29 +6,7 @@
 //     For all details and documentation:
 //     http://backbonejs.org
 
-(function(root, factory) {
-
-  // Set up Backbone appropriately for the environment. Start with AMD.
-  if (typeof define === 'function' && define.amd) {
-    define(['underscore', 'jquery', 'exports'], function(_, $, exports) {
-      // Export global even in AMD case in case this script is loaded with
-      // others that may still expect a global Backbone.
-      root.Backbone = factory(root, exports, _, $);
-    });
-
-  // Next for Node.js or CommonJS. jQuery may not be needed as a module.
-  } else if (typeof exports !== 'undefined') {
-    var _ = require('underscore'), $;
-    try { $ = require('jquery'); } catch(e) {};
-    factory(root, exports, _, $);
-
-  // Finally, as a browser global.
-  } else {
-    root.Backbone = factory(root, {}, root._, (root.jQuery || root.Zepto || root.ender || root.$));
-  }
-
-}(this, function(root, Backbone, _, $) {
-
+(function(root, Backbone, _, $) {
   // Initial Setup
   // -------------
 
@@ -1589,6 +1567,6 @@
     };
   };
 
-  return Backbone;
+  module.exports = Backbone;
 
-}));
+})(exports,{},require('underscore'),require('zepto'));
